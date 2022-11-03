3c15e10a0a6f3feed453ba6493d3e25728179ec5
griddles <89864073+griddles@users.noreply.github.com>
2022-11-03 12:02:45 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace convertToHex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static string HexConverter(string str)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(str);
+            string hexString = BitConverter.ToString(bytes);
+            return hexString.Replace("-", " ");
+        }
+    }
+}
